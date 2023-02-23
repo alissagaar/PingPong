c49b7ce7f2b2d699c769191119b613a42886de6e
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-02-23 15:57:02 -0500

    Get/Set methods for the paddle

---
+++ Paddle.js
@@ -0,0 +1,11 @@
+export default class Paddle{
+    constructor(paddleElem){
+        this.paddleElem = paddleElem
+    }
+    get position(){
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"))
+    }
+    set position(value){
+        this.paddleElem.style.setProperty("--position", value)
+    }
+}
