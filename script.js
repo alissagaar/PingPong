b06035aff61be93f28e03cc46913d5c922db6403
Alissa Gamarra <alissa.gamarra@gmail.com>
2023-02-23 15:48:47 -0500

    Ball Completed

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 import Ball from "./Ball.js";
+import Paddle from "./Paddle.js";
 
 const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
 
 let lastTime
 function update(time){
     if(lastTime != null){
         const delta = time - lastTime
-        ball.update(delta)
+        // ball.update(delta)
     }
 
     lastTime = time
